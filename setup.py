97e9b49dbd7e5ef47013dd02e88492f01b1daf49
Yoav Kleinberger <haarcuba@gmail.com>
2017-12-19 16:11:17 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+README = 'close remote SSH processes automatically'
+
+requires = []
+tests_require = [ 'pytest', ]
+
+setup(name='dryable',
+      version='0.1.0',
+      description=README,
+      long_description=README,
+      url='https://github.com/haarcuba/dryable',
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Yoav Kleinberger',
+      author_email='haarcuba@gmail.com',
+      keywords='subprocess',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points={},
+      )
