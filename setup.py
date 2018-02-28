1c91a7324d1c2ec3ea2dd2a1c0d243a161864797
Yoav Kleinberger <haarcuba@gmail.com>
2018-02-28 16:24:10 +0200

    version 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = []
 tests_require = [ 'pytest', ]
 
 setup(name='dryable',
-      version='0.2.1',
+      version='1.0.0',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/dryable',
