c0fc8c0a8b9d0f3d68c0560ab977334f78c05970
Yoav Kleinberger <haarcuba@gmail.com>
2023-03-03 20:28:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = []
 tests_require = [ 'pytest', ]
 
 setup(name='dryable',
-      version='1.1.0',
+      version='1.2.0',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/dryable',
